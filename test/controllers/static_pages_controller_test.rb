c37519b51e511c0feb75fdca9b5a6f786822b7c4
Salvatore D'Angelo <sasadangelo@gmail.com>
2015-10-09 10:07:32 +0200

    Look and Feel + Signup and Contact pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | SimpleTwitter"
+    assert_select "title", "Simple Twitter"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | SimpleTwitter"
+    assert_select "title", "Help | Simple Twitter"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | SimpleTwitter"
+    assert_select "title", "About | Simple Twitter"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Simple Twitter"
+  end
+  
 end
