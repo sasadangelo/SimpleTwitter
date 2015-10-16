1f31bda097707020320d1030936279b2e2f35764
Salvatore D'Angelo <sasadangelo@gmail.com>
2015-10-16 17:50:21 +0200

    Added edit, list, delet of users and added also admin role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013082536) do
+ActiveRecord::Schema.define(version: 20151016153238) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151013082536) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
